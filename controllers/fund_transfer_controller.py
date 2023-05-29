fda085f6a80ea5037df6eeb32277cf3f2c1666ea
Ramanathan <r-senthil-kumar@hpe.com>
2023-05-29 17:24:18 +0800

    Updated active page

--- controllers/fund_transfer_controller.py
+++ controllers/fund_transfer_controller.py
@@ -8,6 +8,7 @@ usertranscation = Usertranscation()
 
 transfer_ctrl = Blueprint("transfer", __name__, static_folder='static', template_folder='templates')
 
+act_page = 'transfer'
 
 def transfer_funds(account_name, account_number, source): 
     user_session = session.get('name')
@@ -35,28 +36,28 @@ def transfer_funds(account_name, account_number, source):
             if not accname_found and not accnumber_found and not transamount_in:
                 msg = "Please Enter Valid Details"
                 if source == 'onetimetransfer' :
-                    return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                    return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                 else:
                     return (transfer(msg))
                 
             if not accname_found:
                 msg = "Account Name not found"  
                 if source == 'onetimetransfer' :
-                    return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                    return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                 else:
                     return (transfer(msg))
                                    
             if not accnumber_found:
                     msg = "Account Number not found"
                     if source == 'onetimetransfer' :
-                      return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                      return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                     else:
                       return (transfer(msg))
                       #return render_template('transfer.html', transfermsg = msg, logedin_user = user_session)
             if not transamount_in:
                     msg = "Please enter a valid amount"
                     if source == 'onetimetransfer' :
-                      return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                      return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                     else:
                       return (transfer(msg))
                     
@@ -64,7 +65,7 @@ def transfer_funds(account_name, account_number, source):
             if accnumber_in == current_user_found["Accno"]:
                     msg = "Cannot Transfer to Self"
                     if source == 'onetimetransfer' :
-                      return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                      return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                     else:
                       return (transfer(msg))
 
@@ -74,7 +75,7 @@ def transfer_funds(account_name, account_number, source):
                 if current_user_before_bal < float(transamount_in):
                     msg = "Insufficient Balance"
                     if source == 'onetimetransfer' :
-                      return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                      return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                     else:
                       return (transfer(msg))
             
@@ -83,7 +84,7 @@ def transfer_funds(account_name, account_number, source):
                 if end_user_account_status == 'Suspended' or end_user_account_status == 'Pending':
                     msg = "Account is " + end_user_account_status + ". Transfer Failed"
                     if source == 'onetimetransfer' :
-                        return render_template('onetime-transfer.html', transfermsg = msg, logedin_user = user_session)
+                        return render_template('onetime-transfer.html', active_page = act_page, transfermsg = msg, logedin_user = user_session)
                     else:
                         return (transfer(msg))
                   
@@ -120,7 +121,7 @@ def transfer_funds(account_name, account_number, source):
                         current_user_found = Userdata.get_data_one({'userid': user_session})
                         current_user_updated_bal = current_user_found["Accbal"]
                         usertranscation.save(col_transactions_currentuser, {"userid":user_session,"Name":current_user_name,"Accno":current_user_accno,"TransID":trans_id,"Fromaccname":current_user_name,"Fromaccno":current_user_accno, "Toaccno":accnumber_in,"Amount":transamount_in,"Transtype":"dr","Date":current_date,"Time":current_time,"Accbal":current_user_updated_bal,"Transdate": datetime.now()} )
-                        return render_template('transfer-sucess.html', transamount = transamount_in, accname=accname_in, accnumber = accnumber_in, transcation_id = trans_id, logedin_user = user_session)
+                        return render_template('transfer-sucess.html', active_page = act_page, transamount = transamount_in, accname=accname_in, accnumber = accnumber_in, transcation_id = trans_id, logedin_user = user_session)
 
 def user_benificary_list(found_list, new_list):
     for i in found_list:
@@ -159,7 +160,6 @@ def transfer(transfer_message=''):
         return render_template('index.html')
     else:
         user_beneficiary_found = Beneficiarydetails.get_user_accno(user_session)
-        act_page = 'transfer'
         if not user_beneficiary_found:
             #print("No Record Found")
             msg = 'No Beneficiary Found for ' +  user_session + '. Please add Beneficiary or use one time Transfer.'
@@ -179,8 +179,7 @@ def add_beneficiary():
         return render_template('index.html')
     else:
         user_session = session.get('name')
-        act_page = 'transfer'
-        return render_template('add-beneficiary.html', logedin_user = user_session)
+        return render_template('add-beneficiary.html', active_page = act_page, logedin_user = user_session)
 
 @transfer_ctrl.route("/api/v1/add-beneficiary",methods=["POST", "GET"])
 def api_add_beneficiary(): 
@@ -203,30 +202,30 @@ def api_add_beneficiary():
             current_user_accno = current_user_found["Accno"]
             if not accname_found and not accnumber_found:
                 msg = "Please Enter Valid Details"
-                return render_template('add-beneficiary.html', add_beneficiary_msg = msg, logedin_user = user_session)
+                return render_template('add-beneficiary.html', active_page = act_page, add_beneficiary_msg = msg, logedin_user = user_session)
 
             if not accname_found:
                 msg = "Account Name not found"
-                return render_template('add-beneficiary.html', add_beneficiary_msg = msg, logedin_user = user_session)
+                return render_template('add-beneficiary.html', active_page = act_page, add_beneficiary_msg = msg, logedin_user = user_session)
             if not accnumber_found:
                  msg = "Account Number not found"
-                 return render_template('add-beneficiary.html', add_beneficiary_msg = msg, logedin_user = user_session)
+                 return render_template('add-beneficiary.html', active_page = act_page, add_beneficiary_msg = msg, logedin_user = user_session)
             if current_user_accno == accnumber_found["Accno"]:
                  print("Current user account found is: ", current_user_accno)
                  print("Account number of 3rd party : ", accnumber_found["Accno"] )
                  msg = "Cannnot Add Self Acc as Beneficiary"
-                 return render_template('add-beneficiary.html', add_beneficiary_msg = msg, logedin_user = user_session)
+                 return render_template('add-beneficiary.html', active_page = act_page, add_beneficiary_msg = msg, logedin_user = user_session)
             
             if accname_in == accname_found["Name"] and accnumber_in == accnumber_found["Accno"]:
                 user_beneficary_found = Beneficiarydetails.get_data_accno('userid', user_session, 'Accno', accnumber_in)
                 #print("Current user benificary found is: ", user_beneficary_found)
                 if user_beneficary_found:
                     msg =  accnumber_in + " is already added as Beneficiary"
-                    return render_template('add-beneficiary.html', add_beneficiary_msg = msg, logedin_user = user_session)
+                    return render_template('add-beneficiary.html', active_page = act_page, add_beneficiary_msg = msg, logedin_user = user_session)
                 else:
                     #update current user Beneficiary list
                     Beneficiarydetails.save({"userid":user_session,"Name":accname_in,"Accno":accnumber_in})
-                    return render_template('add-beneficiary-sucess.html', accname=accname_in, accnumber = accnumber_in, logedin_user = user_session)
+                    return render_template('add-beneficiary-sucess.html', active_page = act_page, accname=accname_in, accnumber = accnumber_in, logedin_user = user_session)
     return render_template('add-beneficiary.html', logedin_user = user_session)
 
 
@@ -244,13 +243,13 @@ def delete_beneficiary():
         if not user_beneficiary_found:
             #print("No Record Found")
             msg = 'No Beneficiary Found for ' +  user_session + '. Please add Beneficiary or use one time Transfer.'
-            return render_template('delete-beneficiary.html', messages1 = msg, logedin_user = user_session)
+            return render_template('delete-beneficiary.html', active_page = act_page, messages1 = msg, logedin_user = user_session)
         else:
             beneficiary_list1 = []
             print("In delete benificary")
             msg = 'Please select Beneficiary to delete'
             final_beneficiary_list = user_benificary_list(user_beneficiary_found, beneficiary_list1 )
-            return render_template('delete-beneficiary.html', data = final_beneficiary_list, messages = msg, logedin_user = user_session)
+            return render_template('delete-beneficiary.html', active_page = act_page, data = final_beneficiary_list, messages = msg, logedin_user = user_session)
 
 @transfer_ctrl.route("/api/v1/delete-beneficiary",methods=["POST", "GET"])
 def api_delete_beneficiary(): 
@@ -265,7 +264,7 @@ def api_delete_beneficiary():
             user_confirmation_input = request.form.get("confirmation")
             if user_beneficiary_input == 'Choose Account:':
                 msg = 'Please select Beneficiary'
-                return render_template('delete-beneficiary.html', deletemsg = msg, messages = msg, logedin_user = user_session)
+                return render_template('delete-beneficiary.html', active_page = act_page, deletemsg = msg, messages = msg, logedin_user = user_session)
             else:
                 user_beneficiary_list = user_beneficiary_input.split("-")
                 accname_in = user_beneficiary_list[0]
@@ -275,10 +274,10 @@ def api_delete_beneficiary():
                 if user_confirmation_input == 'yes':
                     #print ("Cmd is :", Beneficiarydetails.delete_data(user_session, accnumber_in))
                     Beneficiarydetails.delete_data(user_session, accnumber_in)
-                    return render_template('del-beneficiary-sucess.html', accname=accname_in, accnumber = accnumber_in, logedin_user = user_session)
+                    return render_template('del-beneficiary-sucess.html', active_page = act_page, accname=accname_in, accnumber = accnumber_in, logedin_user = user_session)
                 else:
                     msg = "Please type yes"
-                    return render_template('delete-beneficiary.html', messages = msg, deletemsg = msg, logedin_user = user_session)
+                    return render_template('delete-beneficiary.html', active_page = act_page, messages = msg, deletemsg = msg, logedin_user = user_session)
     return render_template('delete-beneficiary.html')
 
         
